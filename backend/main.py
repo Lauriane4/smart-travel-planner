cf91892139d9756111cfc0a279d0ec9b2e1f1ad0
Lau <laurianetutalamo@gmail.com>
2026-01-08 23:59:44 -0500

    fix: configuration du middleware CORS pour la communication front-back
    - Implémentation de CORSMiddleware dans FastAPI.
    
    - Autorisation des requêtes entre le frontend (port 80) et le backend (port 8000).
    
    - Configuration des en-têtes de sécurité pour le développement local.

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI() 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 geolocator = Nominatim(user_agent="travel_planner", timeout=10)
